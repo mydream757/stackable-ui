23e6304287c785034942112ca8b025f05101abad
Joonyoung Choi <mydream757@gmail.com>
2022-03-27 21:19:41 +0900

    update/ storybook 웹팩 설정 수정
    Problems
    * storybook의 main.js 의 웹팩 설정 문제로 인해, 모듈에 대한 경로가 제대로 resolving 되지 않아 실행 시 오류 발생
    
    Solve
    * 프로젝트 웹팩 설정과 마찬가지로, alias를 설정하여 import 경로를 의도에 맞게 파싱할 수 있도록 수정

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,34 +1,35 @@
-const path = require('path')
-const toPath = _path => path.join(process.cwd(), _path)
+const path = require('path');
+const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    'storybook-css-modules-preset',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "webpackFinal":
-      async (config) => {
-        config.module.rules.push({
-          test: /\.(ts|tsx)$/,
-          loader: require.resolve('babel-loader'),
-          options: {
-            presets: [
-              require.resolve('@babel/preset-typescript'),
-              require.resolve('@emotion/babel-preset-css-prop'),
-            ],
-          },
-        });
-        config.resolve.extensions.push('.ts', '.tsx');
-        config.resolve.alias = {
-          ...config.resolve.alias,
-          '@emotion/core': toPath('node_modules/@emotion/react'),
-        };
-        return config;
+  framework: '@storybook/react',
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [
+          require.resolve('@babel/preset-typescript'),
+          require.resolve('@emotion/babel-preset-css-prop'),
+        ],
       },
-}
+    });
+    config.resolve.mainFields = ['browser', 'module', 'main'];
+    config.resolve.modules = [...config.resolve.modules];
+    config.resolve.extensions.push('.ts', '.tsx');
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      node_modules: path.resolve(__dirname, '../node_modules'),
+      src: path.resolve(__dirname, '../src'),
+      '@emotion/core': toPath('node_modules/@emotion/react'),
+    };
+    return config;
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -5,42 +5,43 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    devServer: {
-        host: 'localhost',
-        hot: true,
-        liveReload: true,
-        open: true,
-        port: 3000
-    },
-    devtool: 'inline-source-map',
-    entry: './src/index.tsx',
-    mode: "development",
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                exclude: /node_modules/,
-                loader: 'ts-loader',
-                test: /\.tsx?$/
-            },
-        ],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
+  devServer: {
+    host: 'localhost',
+    hot: true,
+    liveReload: true,
+    open: true,
+    port: 3000,
+  },
+  devtool: 'inline-source-map',
+  entry: './src/index.tsx',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+        test: /\.tsx?$/,
+      },
     ],
-    resolve: {
-        alias: {
-            'src': path.resolve(__dirname, './src')
-        },
-        extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  resolve: {
+    alias: {
+      node_modules: path.resolve(__dirname, './node_modules'),
+      src: path.resolve(__dirname, './src'),
     },
+    extensions: ['.tsx', '.ts', '.js'],
+  },
 };
