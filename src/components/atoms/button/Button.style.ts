04f0444affa11f4ae49ebc5b99d498f78ea3045e
Joonyoung Choi <mydream757@gmail.com>
2022-03-27 21:24:53 +0900

    feature/ atoms/Button 컴포넌트 프로토타이핑
    * 버튼의 색상은 디자인시스템에 따라 ButtonColorType 으로 설정하여, 정해진 타입만으로 지정할 수 있도록 설정
    * width 와 height, textSize 의 경우, 필요에 맞게 커스텀할 수 있도록 별도 프로퍼티로 분리

---
+++ src/components/atoms/button/Button.style.ts
@@ -0,0 +1,59 @@
+import { css } from '@emotion/react';
+import { SerializedStyles } from '@emotion/serialize';
+
+export const ButtonCommonStyle: SerializedStyles = css`
+  font-family: 'Arial', '맑은고딕', 'sans-serif';
+  border-radius: 6px;
+  border: none;
+  box-sizing: content-box;
+  font-weight: 500;
+  .button-inner {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const ButtonColorStyleSet = {
+  darkPrimary: css`
+    background: linear-gradient(180deg, #4b91f7 0%, #367af6 100%);
+    box-shadow: 0px 0.5px 1.5px rgba(54, 122, 246, 0.25);
+    color: #ffffff;
+    &:focus {
+      border: 3.5px solid rgba(81, 163, 240, 0.5);
+      linear-gradient(180deg, #3B7ADD 0%, #346DC5 100%);
+    }
+  `,
+  gray: css`
+    background: #6e6d70;
+    border: 0.5px solid rgba(0, 0, 0, 0.12);
+    box-shadow: 0px 0.5px 1px rgba(0, 0, 0, 0.1),
+      inset 0px 0.5px 0.5px rgba(255, 255, 255, 0.5);
+    &:focus {
+      border: 3.5px solid rgba(58, 108, 217, 0.5);
+    }
+
+    color: #dfdedf;
+  `,
+  primary: css`
+    background: linear-gradient(180deg, #4b91f7 0%, #367af6 100%);
+
+    box-shadow: 0px 0.5px 1.5px rgba(54, 122, 246, 0.25);
+    color: #ffffff;
+    &:focus {
+      border: 3.5px solid rgba(58, 108, 217, 0.5);
+    }
+  `,
+  white: css`
+    background: #ffffff;
+    border: 0.5px solid rgba(0, 0, 0, 0.12);
+    box-shadow: 0px 0.5px 1px rgba(0, 0, 0, 0.1);
+
+    &:focus {
+      border: 3.5px solid rgba(58, 108, 217, 0.5);
+    }
+    color: #3d3d3d;
+  `,
+};
+
+export type ButtonColorType = keyof typeof ButtonColorStyleSet;
---
+++ src/components/atoms/button/Button.tsx
@@ -0,0 +1,47 @@
+import { css } from '@emotion/react';
+import React, { ReactElement, ReactNode } from 'react';
+import {
+  ButtonColorStyleSet,
+  ButtonColorType,
+  ButtonCommonStyle,
+} from 'src/components/atoms/button/Button.style';
+import { Length } from 'src/config/types/length';
+
+export interface ButtonProps {
+  className?: string;
+  children: ReactNode;
+  onClick?: () => void;
+  colorType?: ButtonColorType;
+  width?: Length;
+  height?: Length;
+  textSize?: Length;
+}
+
+export function Button({
+  className,
+  children,
+  onClick,
+  colorType = 'primary',
+  width = '84px',
+  height = '25px',
+  textSize = '14px',
+}: ButtonProps): ReactElement {
+  return (
+    <button
+      className={'button-outer'}
+      css={[
+        ButtonCommonStyle,
+        ButtonColorStyleSet[colorType],
+        css`
+          width: ${width};
+          height: ${height};
+          font-size: ${textSize};
+        `,
+        className,
+      ]}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
