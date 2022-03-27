b53f3b690b0cd4d4bf2320e972fea3477420145c
Joonyoung Choi <mydream757@gmail.com>
2022-03-27 21:22:43 +0900

    feature/ Length 타입 프로토타이핑
    * 길이를 타입 체킹할 수 있도록 Length 타입 프로토타이핑

---
+++ src/config/types/length.ts
@@ -0,0 +1,16 @@
+export type Pixel = `${number}px`;
+export type Percent = `${number}%`;
+export type Em = `${number}em`;
+export type Rem = `${number}rem`;
+export type VerticalHeight = `${number}vh`;
+export type VerticalWidth = `${number}vw`;
+export type AbstractLength = 'auto' | 'inherit';
+
+export type Length =
+  | Pixel
+  | Percent
+  | Em
+  | Rem
+  | VerticalHeight
+  | VerticalWidth
+  | AbstractLength;
